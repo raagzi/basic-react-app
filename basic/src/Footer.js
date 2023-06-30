32e03450f62628e554c597738fe7832483374f2e
raagzi <114258169+raagzi@users.noreply.github.com>
2023-06-30 23:25:20 +0530

    Update Footer.js

--- basic/src/Footer.js
+++ basic/src/Footer.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import './Footer.css'
 
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span className="text-muted">© 2023 Raksha</span>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
